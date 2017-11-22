d1a737a0d1e601542c3e2c3428921568a7e4aafe
Alex Vance <alexdvance@gmail.com>
2017-11-21 21:17:13 -0800

    Map shows up

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,4 +12,37 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', [function() {
   console.log('hey')
   this.test = 'Test';
+
+  var width = 960;
+  var height = 500;
+
+  var svg = d3.select('#sf-map')
+    .attr('width', width)
+    .attr('height', height);
+  var projection = d3.geoAlbersUsa();
+  var path = d3.geoPath().projection(projection);
+
+  d3.json('/resources/sfmaps/streets.json', function(error, sf) {
+    console.log('sf', sf)
+    if (error) throw error;
+
+    projection
+      .scale(1)
+      .translate([0, 0]);
+
+    // Compute the bounds of a feature of interest, then derive scale & translate.
+    // https://stackoverflow.com/a/14691788
+    var b = path.bounds(sf),
+        s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+    projection
+      .scale(s)
+      .translate(t);
+
+    svg.append('path')
+      .datum(sf)
+      .attr('stroke', 'blue')
+      .attr('d', path);
+  });
 }]);
