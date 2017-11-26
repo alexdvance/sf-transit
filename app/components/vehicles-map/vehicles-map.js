47ad5d3e42e1f9822102c8a1591560076328870d
Alex Vance <alexdvance@gmail.com>
2017-11-26 14:42:39 -0800

    Moved map creation calls to vehicles-map component

--- app/components/vehicles-map/vehicles-map.js
+++ app/components/vehicles-map/vehicles-map.js
@@ -1,10 +1,22 @@
-angular.module('muni.vehiclesMap', ['muni.d3Map', 'muni.mapData'])
+angular.module('muni.vehiclesMap', [])
 
 .component('vehiclesMap', {
   templateUrl: 'components/vehicles-map/vehicles-map.html',
   bindings: {},
   controllerAs: 'vm',
-  controller: ['mapData', function(mapData) {
+  controller: ['$element', '$interval', 'd3Map', 'mapData', 'localMapAPI', 'nextBusAPI', function($element, $interval, d3Map, mapData, localMapAPI, nextBusAPI) {
       this.mapData = mapData;
+
+      localMapAPI.get().then(function(sfmap) {
+        d3Map.createGeoJSONMap($element[0], sfmap.data);
+
+        nextBusAPI.getVehicles(mapData.lastTime)
+          .then(d3Map.addVehicleInfo);
+
+        $interval(function() {
+          nextBusAPI.getVehicles(mapData.lastTime)
+            .then(d3Map.addVehicleInfo);
+        }, 15000);
+      });
     }]
 });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,20 +5,5 @@ angular.module('muni.view1', ['ngRoute', 'muni.utils', 'muni.mapData', 'muni.loc
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl as vm'
   });
-}])
-
-.controller('View1Ctrl', ['$interval', 'd3Map', 'mapData', 'localMapAPI', 'nextBusAPI', function($interval, d3Map, mapData, localMapAPI, nextBusAPI) {
-    localMapAPI.get().then(function(sfmap) {
-      d3Map.createGeoJSONMap(sfmap.data);
-
-      nextBusAPI.getVehicles(mapData.lastTime)
-        .then(d3Map.addVehicleInfo);
-
-      $interval(function() {
-        nextBusAPI.getVehicles(mapData.lastTime)
-          .then(d3Map.addVehicleInfo);
-      }, 15000);
-    });
 }]);
