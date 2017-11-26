48dc786808e92188ca73c85711f75d5d105a94e7
Alex Vance <alexdvance@gmail.com>
2017-11-26 14:51:18 -0800

    Added back d3Map injector in view1 module

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('muni.view1', ['ngRoute', 'muni.utils', 'muni.mapData', 'muni.localMapAPI', 'muni.nextBusAPI', 'muni.routeFilter', 'muni.vehiclesMap'])
+angular.module('muni.view1', ['ngRoute', 'muni.utils', 'muni.d3Map', 'muni.mapData', 'muni.localMapAPI', 'muni.nextBusAPI', 'muni.routeFilter', 'muni.vehiclesMap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
