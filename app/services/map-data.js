4005263b37b24bb379f0cd23bc9039ebfd98c07e
Alex Vance <alexdvance@gmail.com>
2017-11-26 17:39:00 -0800

    Changed view1 to sfMap

--- app/services/map-data.js
+++
@@ -1,9 +0,0 @@
-angular.module('muni.mapData', [])
-
-.service('mapData', [function() {
-  this.routes = [];
-  this.vehiclesHash = {};
-  this.filterHash = {};
-  this.filtersOn = false;
-  this.lastTime = 0;
-}]);
--- app/view1/view1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('muni.view1', ['ngRoute', 'muni.utils', 'muni.d3Map', 'muni.mapData', 'muni.localMapAPI', 'muni.nextBusAPI', 'muni.routeFilter', 'muni.vehiclesMap'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'view1/view1.html',
-  });
-}]);
